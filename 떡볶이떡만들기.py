79448e10d6396c41646176468e621b27986b70e2
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2022-12-06 21:01:30 +0900

    Create 떡볶이떡만들기.py

---
+++ 떡볶이떡만들기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+x = list(map(int,input().split()))
+
+start = 0
+end = max(x)
+result = 0
+while (start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for i in x:
+        if i > mid:
+            total += (i - mid)
+    if total < m :
+        end = mid -1
+    elif total >= m :
+        result = mid
+        start = mid + 1
+print(result)

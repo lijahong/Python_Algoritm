8e9c55617756a0a705f7ec4727cbeceac41309cf
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2023-05-09 22:47:11 +0900

    Update 1912dp.py

--- 1912dp.py
+++ 1912dp.py
@@ -13,3 +13,16 @@ for i in range(len(arr)-1):
     answerarr.append(max(answerarr[i]+arr[i+1],arr[i+1]))
     
 print(max(answerarr))
+
+# for 문 범위만 따로 설정한 풀이
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+answerarr = [arr[0]] + [0] * (n-1)
+for i in range(1,n):
+    answerarr[i] = max(answerarr[i-1] + arr[i] , arr[i])
+print(max(answerarr))

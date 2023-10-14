4d938861a7d969488124c4dbaa4cd565c711012a
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2023-10-14 21:30:10 +0900

    [Silver I] Title: 경로 찾기, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,19 +1,26 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
+
 n = int(input())
 graph = [list(map(int,input().split())) for _ in range(n)]
+
 canvisit = [[0]*n for _ in range(n)]
+nearvisit = [[] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            nearvisit[i].append(j)
 def bfs(i):
     visit = [False] * n
     queue = deque([i])
     while queue:
         node = queue.popleft()
-        for j in range(n):
-            if visit[j] == False and graph[node][j] == 1:
+        for j in nearvisit[node]:
+            if visit[j] == False:
                 queue.append(j)
-                canvisit[i][j] = 1
                 visit[j] = True
+                canvisit[i][j] = 1
 for i in range(n):
     bfs(i)
 for i in canvisit:

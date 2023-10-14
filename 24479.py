c8601bf3777b719a45f79fe4faae7f1264d64733
JAE HONG LEE <69387517+lijahong@users.noreply.github.com>
2023-10-14 18:22:05 +0900

    Update 24479.py

--- 24479.py
+++ 24479.py
@@ -28,3 +28,30 @@ count = 0
 dfs(a,r,b)
 for i in range(1, n + 1):
     print(a[i])
+
+
+###################### 다른 풀이 ##########################
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**8)
+
+n,m,r = map(int,input().split())
+graph = [[] for _ in range(n)]
+visit = [ 0 for _ in range(n)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a - 1].append(b - 1)
+    graph[b - 1].append(a - 1)
+
+count = 0
+def dfs(node):
+    global count
+    count += 1
+    visit[node] = count
+    graph[node].sort()
+    for i in graph[node]:
+        if visit[i] == 0:
+            dfs(i)
+dfs(r -1)
+for i in visit:
+    print(i)
